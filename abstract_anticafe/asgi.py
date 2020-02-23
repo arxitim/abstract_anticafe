081e7184c7160c4836475b749c5a1b757b5d0d54
arxitim <arseniy22848@gmail.com>
2020-02-23 21:33:57 +0300

    initial commit

---
+++ abstract_anticafe/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for abstract_anticafe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abstract_anticafe.settings')
+
+application = get_asgi_application()
---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+
+class HomePage(View):
+    def get(self, request):
+        return render(request, 'core/index.html', context={})
+
+
+class PageNotFound(View):
+    def get(self, request):
+        return HttpResponse("Whoops, page not found")
