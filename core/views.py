b66f2f40770829bfb509ef8e92f1ecb0f7bac254
arxitim <arseniy22848@gmail.com>
2020-02-28 19:02:48 +0300

    dynamic render for tables added (for...)

--- core/views.py
+++ core/views.py
@@ -5,7 +5,9 @@ from django.views import View
 
 class HomePage(View):
     def get(self, request):
-        return render(request, 'core/index.html', context={})
+        return render(request, 'core/index.html', context={'tables': ['table_1', 'table_2',
+                                                                      'table_3', 'table_4',
+                                                                      'table_5', 'table_6']})
 
 
 class PageNotFound(View):
