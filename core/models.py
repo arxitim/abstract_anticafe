573e35dcbc5f09805d7f68435b8ab787f9fdffcf
arxitim <arseniy22848@gmail.com>
2020-03-04 23:27:54 +0300

    test with registration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True, unique=True, db_index=True)
+    password = models.CharField(max_length=128, validators=[validators.MinLengthValidator(1)])
+    name = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=128)
+    email = models.EmailField(max_length=128)
+    tg_nick = models.CharField(max_length=128)
+
